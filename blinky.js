83f8937e719fd5e9e41daa4f77f4e83b8d89323d
Corey Menscher <clmensch@gmail.com>
2013-04-19 01:55:59 -0400

    API working
    - Create a method for getUser
    - Create a method for deviceList
    - Create a method for getMeasure
    - The current version uses getMeasure to grab the last Sound Pressure Level value (Noise) and lights the LED for three ranges of sound levels.

--- blinky.js
+++ blinky.js
@@ -3,11 +3,14 @@ var blink1;
 
 
 /*  Use the "Client Credentials" Auth */
+var http = require('http');
 var https = require('https');
 var querystring = require('querystring');
 
 
 var config = {
+    loop_interval: 8000, // 8sec
+
     auth_request: {
         grant_type: "password",
         client_id: [CLIENT_ID],
@@ -24,11 +27,11 @@ var config = {
     },
 
     auth_options: {
-        hostname: 'api.netatmo.net',
+        hostname: "api.netatmo.net",
         headers: {"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8", "Content-Length": 0},
         port: 443,
-        path: '/oauth2/token',
-        method: 'POST '
+        path: "/oauth2/token",
+        method: "POST"
     },
 
     credentials: {
@@ -39,12 +42,19 @@ var config = {
         "refresh_token": ""
     },
 
+    api_options: {
+        hostname: "api.netatmo.net",
+        headers: {"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"},
+    },
+
     nextTokenRefresh: 0,    
 }
 
 
 var app = {
     startTime: 0,
+
+    lastValue: 0, // holds the last value of whatever we decide to examine (i.e. Temp, Pressure, Humidity, Sound...)
 
     getToken: function(callback) {
         var _this = this;
@@ -61,8 +71,8 @@ var app = {
         config.auth_options.headers = {"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8", "Content-Length": auth_data.length};
 
         var req = https.request(config.auth_options, function(res) {
-          console.log("statusCode: ", res.statusCode);
-          console.log("headers: ", res.headers);
+          //console.log("statusCode: ", res.statusCode);
+          //console.log("headers: ", res.headers);
 
           res.on('data', function(d) {
             var cred_obj = JSON.parse(d);
@@ -103,8 +113,8 @@ var app = {
         config.auth_options.headers = {"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8", "Content-Length": auth_data.length};
 
         var req = https.request(config.auth_options, function(res) {
-            console.log("statusCode: ", res.statusCode);
-            console.log("headers: ", res.headers);
+            //console.log("statusCode: ", res.statusCode);
+            //console.log("headers: ", res.headers);
 
             res.on('data', function(d) {
                 var cred_obj = JSON.parse(d);
@@ -133,11 +143,10 @@ var app = {
     refreshTokenCheck: function() {
         var _this = this;
 
-        console.log("Checking to see if the access token needs to be refreshed...");
+        console.log("\nChecking to see if the access token needs to be refreshed...");
 
         var now = new Date();
         var now_millis = now.getTime() / 1000;
-        console.log(config.nextTokenRefresh + " > " + app.startTime + config.credentials.expires_in );
 
         // Note: adding 1 minute to the expires_in cutoff so we can prevent loop() from being called when the token has expired
         if(config.nextTokenRefresh > (app.startTime + config.credentials.expires_in + 60000)) {
@@ -148,26 +157,152 @@ var app = {
 
     refreshTokenTimer: {},
 
-    init: function() {
+    getUser: function() {
+
+        // Set the method
+        config.api_options.path = "/api/getuser?access_token=" + config.credentials.access_token;
+
+        var req = https.request(config.api_options, function(res) {
+            //console.log("statusCode: ", res.statusCode);
+            //console.log("headers: ", res.headers);
+
+            res.on('data', function(d) {
+                var res = JSON.parse(d);
+
+                //DO SOMETHING
+
+                console.log("\n");
+                //process.stdout.write(d);
+            });
+        });
+
+        req.write("data\n");
+        req.write("data\n");
+        req.end();
+
+        req.on('error', function(e) {
+          console.error(e);
+        });
+    },
+
+   getDevices: function() {
+        // Set the method
+        config.api_options.path = "/api/devicelist?access_token=" + config.credentials.access_token;
+
+        var req = https.request(config.api_options, function(res) {
+            console.log("statusCode: ", res.statusCode);
+            console.log("headers: ", res.headers);
+            console.log("\n");
+
+            res.on('data', function(d) {
+                var res = JSON.parse(d);
+            
+                var _id = res.body.devices[0]._id;
+
+                /* DO SOMETHING */
+
+                console.log("\n");
+
+                //process.stdout.write(d);
+            });
+        });
+
+        req.write("data\n");
+        req.write("data\n");
+        req.end();
+
+        req.on('error', function(e) {
+          console.error(e);
+        });
+    },
+
+    getMeasurement: function(device_id) {
+        var _this = this;
+
+        // Set the method
+        //config.api_options.path = "/api/getmeasure";
+        config.api_options.path = "/api/getmeasure?access_token=" + config.credentials.access_token + "&device_id=" + device_id + "&scale=max&type=Noise&date_end=last";
+
+        var params =  querystring.stringify({
+            "access_token": config.credentials.access_token,
+            "device_id": device_id,
+            "scale": "max",
+            "type": "Noise",
+            "date_end": "last",
+        });
+
+
+        var req = https.request(config.api_options, function(res) {
+            console.log("statusCode: ", res.statusCode);
+            console.log("headers: ", res.headers);
+            console.log("\n");
+
+            res.on('data', function(d) {
+                var res = JSON.parse(d);
+                console.log(res.body[0].value[0][0]);
+                console.log("\n");
+
+                var val = res.body[0].value[0][0];
+                if(_this.lastValue != val) {
+                    _this.lastValue = val;
+                    _this.setRGB();
+                }
+
+                //process.stdout.write(d);
+            });
+        });
+
+        //req.write(params);
+        req.write("data\n");
+
+        req.end();
+
+        req.on('error', function(e) {
+          console.log("\n");
+          console.error(e);
+          console.log("\n");
+        });
+
+    },
+
+    setRGB: function() {
+        var val = this.lastValue;
+        if(val < 45) {
+            blink1.fadeToRGB(500, 0, 0, 255);
+        } else if( val > 45 || val < 52) {
+            blink1.fadeToRGB(500, 0, 255, 0);
+        } else {
+            blink1.fadeToRGB(500, 255, 0, 0);
+        }
+    },
+
+    loop: function() {
+        /* This is the loop that will get the data at the interval specified in the config object */
+        var _this = this;
+        setInterval(function() {
+            console.log("Running...");
+            //_this.getUser();
+            //_this.getDevices();
+            _this.getMeasurement("70:ee:50:00:71:fc");
+        }, config.loop_interval);
+    },
+
+    start: function() {
         var _this = this;
         var now = new Date();
         
         this.startTime = now.getTime() / 1000; // Unix epoch
 
         this.getToken(function() {
-            _this.refreshTokenTimer = setInterval(_this.refreshTokenCheck, 5000);
+            _this.refreshTokenTimer = setInterval(_this.refreshTokenCheck, 10000); // check every 10 sec
         });
 
         
         blink1 = new Blink1.Blink1();
         blink1.fadeToRGB(500, 255, 0, 0, function() { blink1.fadeToRGB(500, 0, 255, 0, function() { blink1.fadeToRGB(500, 9, 0, 255, function() { blink1.fadeToRGB(100, 0, 0, 0);});})})
+
+        this.loop();
     },
 }
 
-
-app.init();
-
-/* This loop will check for updates and set the blink color accordingly... */
-var loop = setInterval(function() {
-    console.log("Running...");
-}, 1000);
+app.start();
