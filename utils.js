75e11328f22a32f7fe61356cbca0290aca347f4e
Corey Menscher <clmensch@gmail.com>
2013-04-29 01:32:05 -0400

    removed utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-var line = 1;
-
-var log = function(msg) {
-    var now = new Date();
-    console.log("(" + line + ") " + now.toDateString() + " " + now.getUTCHours() + ":" + now.getUTCMinutes() + ":" + now.getUTCSeconds() + " --> " + msg);
-    line++;
-};
-
-exports.log = log;
